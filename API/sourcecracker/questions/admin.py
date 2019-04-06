aa85ec2fd2fd41df81459ee6af5a29d7d313bf77
Robert <urb.rob@o2.pl>
2019-04-06 14:39:58 +0200

    Add inlines

--- API/sourcecracker/questions/admin.py
+++ API/sourcecracker/questions/admin.py
@@ -2,14 +2,26 @@ from django.contrib import admin
 from questions.models import Question, Answer, Rating
 
 
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 0
+
+
+class RatingInline(admin.TabularInline):
+    model = Rating
+    extra = 0
+
+
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('id', 'content', 'group')
+    inlines = [AnswerInline]
 
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'url', 'question')
+    inlines = [RatingInline]
 
 
 @admin.register(Rating)
