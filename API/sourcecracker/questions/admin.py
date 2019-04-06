5f7e855ce8c2bb2d0c7d03c390ebc49acdf3b179
Robert <urb.rob@o2.pl>
2019-04-06 14:11:23 +0200

    Add basic admins view

--- API/sourcecracker/questions/admin.py
+++ API/sourcecracker/questions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from questions.models import Question, Answer, Rating
 
-# Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'group')
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url', 'question')
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'rating_type', 'rate', 'user', 'answer')
