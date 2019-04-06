b5801f21d4935db4de84cdc0129d63bdb5e247ef
Robert <urb.rob@o2.pl>
2019-04-06 20:13:29 +0200

    Add login/register mutation

--- API/sourcecracker/accounts/schema.py
+++ API/sourcecracker/accounts/schema.py
@@ -1,5 +1,8 @@
 from graphene_django import DjangoObjectType
 from accounts.models import User, Group, Membership
+from sourcecracker.mixins import MutationErrorMixin
+from django.db.models import Q
+from django.contrib.auth import authenticate
 import graphene
 
 
@@ -18,6 +21,40 @@ class MembershipNode(DjangoObjectType):
         model = Membership
 
 
+class UserRegistrationMutation(MutationErrorMixin, graphene.Mutation):
+    class Arguments:
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    hash_id = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        username = kwargs['email'].split('@')[0]
+        try:
+            User.objects.get(Q(username=username) | Q(email=kwargs['email']))
+        except User.DoesNotExist:
+            user = User.objects.create_user(username=username, email=kwargs['email'], password=kwargs['password'])
+            return UserRegistrationMutation(hash_id=user.hash_id)
+        else:
+            return UserRegistrationMutation.error('Email or username is taken')
+
+
+class UserLoginMutation(MutationErrorMixin, graphene.Mutation):
+    class Arguments:
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    hash_id = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        user = authenticate(username=kwargs['email'].split('@')[0], password=kwargs['password'])
+        if user:
+            return UserLoginMutation(hash_id=user.hash_id)
+        else:
+            return UserLoginMutation.error("Unauthorized attempt")
+
+
+
 class Query(graphene.ObjectType):
     users = graphene.List(UserNode)
 
@@ -26,7 +63,8 @@ class Query(graphene.ObjectType):
 
 
 class Mutation(graphene.ObjectType):
-    pass
+    register_user = UserRegistrationMutation.Field()
+    login_user = UserLoginMutation.Field()
 
 
 schema = graphene.Schema(query=Query)
