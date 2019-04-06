b230e2d6ae05306b2a0f47198548f64f44b996c3
urbrob <35544390+urbrob@users.noreply.github.com>
2019-04-06 19:39:18 +0200

    Add question and questions queries (#16)
    * Add query
    
    * Add question query
    
    * Fix cors/csrf
    
    * Add question query

--- API/sourcecracker/questions/schema.py
+++ API/sourcecracker/questions/schema.py
@@ -1,5 +1,7 @@
 from graphene_django import DjangoObjectType
 from questions.models import Question, Answer, Rating
+from graphene import Argument, Boolean, String, Int
+from django.db.models import Subquery, Count
 from accounts.models import User
 import graphene
 
@@ -9,21 +11,39 @@ class RatingNode(DjangoObjectType):
         model = Rating
 
 
-class AnswerpNode(DjangoObjectType):
+class AnswerNode(DjangoObjectType):
     class Meta:
         model = Answer
 
 
 class QuestionNode(DjangoObjectType):
+    answers = graphene.List(AnswerNode, is_long=Argument(Boolean), is_science=Argument(Boolean), is_complex=Argument(Boolean))
+
     class Meta:
         model = Question
 
+    def resolve_answers(self, info, *args, **kwargs):
+        return self.answer_set.all()
+
 
 class Query(graphene.ObjectType):
-    questions = graphene.List(QuestionNode)
+    questions = graphene.List(
+        QuestionNode,
+        question=Argument(String)
+    )
+    question = graphene.Field(
+        QuestionNode,
+        question_id=Argument(Int, required=True)
+    )
 
-    def resolve_questions(self, info):
-        return Question.objects.all()
+    def resolve_question(self, info, question_id):
+        try:
+            return Question.objects.get(id=question_id)
+        except Question.DoesNotExist:
+            return None
+
+    def resolve_questions(self, info, **kwargs):
+        return Question.objects.filter(content__icontains=kwargs.get('question', ''))
 
 
 class CreateQuestion(graphene.Mutation):
