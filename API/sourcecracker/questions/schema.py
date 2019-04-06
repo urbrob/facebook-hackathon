78287505e9c9e45693095f7a9c71cf3482a56960
Pimkey <Pimkey@users.noreply.github.com>
2019-04-06 17:59:08 +0200

    Creating Question (#15)

--- API/sourcecracker/questions/schema.py
+++ API/sourcecracker/questions/schema.py
@@ -1,5 +1,6 @@
 from graphene_django import DjangoObjectType
 from questions.models import Question, Answer, Rating
+from accounts.models import User
 import graphene
 
 
@@ -25,8 +26,21 @@ class Query(graphene.ObjectType):
         return Question.objects.all()
 
 
+class CreateQuestion(graphene.Mutation):
+    class Arguments:
+        question = graphene.String(required=True)
+        hash_id = graphene.String(required=True)
+
+    question = graphene.Field(QuestionNode)
+
+    def mutate(self, info, *arg, **kwargs):
+        user = User.objects.get(hash_id=kwargs['hash_id'])
+        question = Question.objects.create(content=kwargs['question'], created_by=user)
+        return CreateQuestion(question=question)
+
+
 class Mutation(graphene.ObjectType):
-    pass
+    create_question = CreateQuestion.Field()
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
